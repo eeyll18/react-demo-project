c44e61a751846f444df901415b864bf944b26e6a
eeyll18 <edaeylulgunay2@gmail.com>
2026-04-22 01:54:40 +0300

    implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MenuPage from "./pages/MenuPage";
+import AppLayout from "./components/AppLayout";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <main className="app-shell">
-      <MenuPage />
-    </main>
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route index element={<MenuPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Route>
+    </Routes>
   );
 }
 
